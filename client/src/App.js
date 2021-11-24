603b6d386ff5319751987641e4b3bb05cc17a9c8
Thomas Cherret <thomas.cherret@papernest.com>
2021-11-24 19:06:44 +0100

    fix test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Working in prod!
+            Work in prod!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
