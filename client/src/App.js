3527ef04b156ba394dd2fc5535184a3a5bce96d8
Thomas Cherret <thomas.cherret@papernest.com>
2021-12-04 19:35:33 +0100

    push

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Deployment with Kubernetes
+            Learn Deployment with Kubernetes, yes!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
