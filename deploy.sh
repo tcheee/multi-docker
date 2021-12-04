0a32e590f66ebada9276e76eb86e115742d5b6dc
Thomas Cherret <thomas.cherret@papernest.com>
2021-12-04 19:09:28 +0100

    change th efront

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ docker push tcheee42/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-#kubectl set image deployments/server-deployment server=tcheee42/multi-client:$SHA
+kubectl set image deployments/server-deployment server=tcheee42/multi-client:$SHA
 kubectl set image deployments/client-deployment server=tcheee42/multi-server:$SHA
 kubectl set image deployments/worker-deployment server=tcheee42/multi-worker:$SHA
